bb4fd6b1e98d986b87c229a867f796feb76d138d
Kody Mullis <kodyamullis@gmail.com>
2019-01-07 16:48:59 -0500

    Created the template, and routers, created partials

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var express = require("express"),
 app.set("view engine","ejs")
 
 app.get("/",function(req,res){
-
   res.render("home")
 })
 
-app.listen(300,function(req,res){
+app.get("/about",function(req,res){
+  res.render("about")
+})
 
+app.get("/contact",function(req,res){
+  res.render("contact")
+})
+
+app.listen(300,function(req,res){
   console.log("server started...");
 })
